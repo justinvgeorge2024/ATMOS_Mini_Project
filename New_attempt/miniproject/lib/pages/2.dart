7de13b44e7d59855e54c7feba73d797ada98a1e0
JJJ164 <joju436@gmail.com>
2023-06-20 00:15:58 +0530

    3

---
+++ New_attempt/miniproject/lib/pages/2.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
+import 'dart:convert';
+import 'package:csv/csv.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final db = FirebaseFirestore.instance;
+
+List<List<String>> itemList = [];
+
+class ItemScreen extends StatefulWidget {
+  const ItemScreen({super.key});
+
+  @override
+  State<ItemScreen> createState() => _ItemScreenState();
+}
+
+class _ItemScreenState extends State<ItemScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  Stream<QuerySnapshot> streamQuery =
+      db.collection("Users").doc(User.email).snapshots();
+  @override
+  void initState() {
+    super.initState();
+    itemList = [
+      <String>["name", "email", "admno"]
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("to export"),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: streamQuery,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Container(
+              child: Column(children: <Widget>[
+                Expanded(
+                    child: ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot doc = snapshot.data!.docs[index];
+                    itemList.add(<String>[
+                      doc.get("name"),
+                      doc.get('email'),
+                      doc.get('admno')
+                    ]);
+                  },
+                ))
+              ]),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.file_download),
+          onPressed: () => {
+                genCsv(),
+              }),
+    );
+  }
+}
+
+genCsv() async {
+  print("gen csv");
+  String csvData = ListToCsvConverter().convert((itemList));
+  DateTime now = DateTime.now();
+  String formattedDate = DateFormat('MM-dd-yyyy-HH-mm-ss').format(now);
+  Directory generalDownloadDir = Directory("storage/emulated.0/Download");
+  final File file =
+      await (File('${generalDownloadDir.path}/item_expot_$formattedDate.csv')
+          .create());
+  await file.writeAsString(csvData);
+}
